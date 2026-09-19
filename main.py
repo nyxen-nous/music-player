2302eb076bb658358dc86230dcef77863a155577
Sammi Zafar <your-sammizafar20l@gmaile.com>
2026-09-19 12:08:02 +0530

    Save latest changes

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ def main():
     mp3_files = [file for file in os.listdir(folder) if file.endswith(".mp3")]
     if not mp3_files:
         print("No .mp3 files found!")
+        return
 
     while True:
         print("***** MP3 PLAYER*****")
@@ -58,7 +59,7 @@ def main():
         for index, song in enumerate(mp3_files, start = 1):
             print(f"{index}. {song}")
 
-        choice_input = input("\nEnter the song # to play (or 'Q' tp quit): ")
+        choice_input = input("\nEnter the song # to play (or 'Q' to quit): ")
 
         if choice_input.upper() == "Q":
             print("Bye!")
